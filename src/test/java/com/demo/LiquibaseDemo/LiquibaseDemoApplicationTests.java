833b2e77a22be72f3dab83a2c9bb539caf0c334d
Abhishek <abhi30996@gmail.com>
2020-01-03 13:26:04 +0000

    Project Setup

---
+++ src/test/java/com/demo/LiquibaseDemo/LiquibaseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.LiquibaseDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
